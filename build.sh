98d2ccf739ca6219efd934bc35e2e014304a987d
ItoWindsor <itowindsor@gmail.com>
2025-03-28 00:40:34 +0100

    CHORE : Update conan, cmakelist, bash script to take to resolve conflicts

--- build.sh
+++ build.sh
@@ -3,11 +3,14 @@ set -e  # Exit immediately if a command fails
 
 BUILD_DIR="cmake-build-debug"
 
+echo "Deleting the old stuff"
+rm -rf cmake-build-debug
+
 echo "📦 Installing dependencies with Conan..."
 conan install . --output-folder=$BUILD_DIR --build=missing
 
-echo "⚙️ Running CMake..."
-cmake -S . -B $BUILD_DIR -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+echo "⚙️ Running CMake with Unix Makefiles..."
+cmake -S . -B $BUILD_DIR -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 
 echo "🔨 Building project..."
 cmake --build $BUILD_DIR
