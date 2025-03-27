dee9f9a3925e70e1486d67328a7a9c4896b3e2e6
ItoWindsor <itowindsor@gmail.com>
2025-03-27 18:08:46 +0100

    CHORE : updated main.cpp

--- main.cpp
+++ main.cpp
@@ -6,4 +6,6 @@ int main() {
     std::cout << "Hello, World!\n";
     DataFrame df = DataFrame::read_csv("data/house-prices-advanced-regression-techniques/train.csv");
     df.head(10);
+    std::cout << "tail" << std::endl;
+    df.tail(10);
 }
