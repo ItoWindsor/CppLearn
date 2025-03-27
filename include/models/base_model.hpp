94d2248842da5cf1a866c9e9046bbda1a1f6436b
ItoWindsor <itowindsor@gmail.com>
2025-03-27 23:40:05 +0100

    UPDATE [Feature|Model] : went from std::vector to Eigen

--- include/models/base_model.hpp
+++ include/models/base_model.hpp
@@ -2,14 +2,20 @@
 #define BASE_MODEL_HPP
 
 #include <vector>
+#include <Eigen/Dense>
 
 class BaseModel {
     public:
         virtual ~BaseModel() = default;
-        virtual void fit(const std::vector<std::vector<double>>& X, const std::vector<double>& y) = 0;
-        virtual std::vector<double> predict(const std::vector<std::vector<double>>& X) const = 0;
+        virtual void fit(const Eigen::MatrixXd& X, const Eigen::VectorXd& y, std::string method) = 0;
+        Eigen::VectorXd predict(const Eigen::MatrixXd& X) const = 0;
         virtual double score(const std::vector<std::vector<double>>& X, const std::vector<double>& y) const = 0;
-  private:
+
+        virtual void save_model(const std::string& filename) const;
+        virtual void load_model(const std::string& filename);
+
+        virtual void print_parameters() const = 0;
+    private:
 };
 
 #endif
---
+++ src/models/base_model.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+#include "models/base_model.hpp"
+
