76e13effcc865c0ed2116a54e75bd637c63b7397
ItoWindsor <itowindsor@gmail.com>
2025-03-30 16:34:28 +0200

    CHORE : update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 #include "core/dataframe.hpp"
+#include <models/linear_regression.hpp>
 
 int main() {
-    std::cout << "Hello, World!\n";
-    DataFrame df = DataFrame::read_csv("data/house-prices-advanced-regression-techniques/train.csv");
-    df.head(10);
+  
+  DataFrame df = DataFrame::read_csv("data/linear_regression_data.csv"); 
+
+  std::vector<std::string> features = {"Feature_1", "Feature_2", "Feature_3", "Feature_4", "Feature_5"};
+  DataFrame sub_df = df[features];
+  Eigen::MatrixXd mat_X = sub_df.get_values();
+  Eigen::MatrixXd mat_y = df[{"Target"}].get_values();
+  
+  std::cout << "mat_X " << std::endl;
+  std::cout << "number of rows : " << mat_X.rows() << std::endl;
+  std::cout << "number of col : " << mat_X.cols() << std::endl;
+  
+  std::cout << "mat_y " << std::endl;
+  std::cout << "number of rows : " << mat_y.rows() << std::endl;
+  std::cout << "number of col : " << mat_y.cols() << std::endl;
+  LinearRegression model;
+  model.fit(mat_X,mat_y);
+  model.print_parameters();
 }
+
+
